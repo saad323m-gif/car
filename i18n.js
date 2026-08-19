cfaedfc0c3d47b2367a96e1f17ee09ac58d8d8cd
saad323m-gif <saad323m@gmail.com>
2026-08-19 22:35:19 +0400

    Delete i18n.js

--- i18n.js
+++
@@ -1,6 +0,0 @@
-// اختبار بسيط
-console.log('i18n.js loaded successfully');
-export const t = (key) => key;
-export const getLanguage = () => 'en';
-export const setLanguage = (lang) => { localStorage.setItem('lang', lang); };
-export const getActionTypeTranslation = (type) => type;
